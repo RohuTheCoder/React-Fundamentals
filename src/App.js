50669cc8c7036a67954c326bb7502ba9e6b59e4c
Rohini G B <gbrohini840@gmail.com>
2025-12-31 12:05:51 +0530

    1st commit : greet component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello,Rohu</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+       <Greet></Greet>
+      </div>
+    );
+  }
 }
 
 export default App;
