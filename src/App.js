c577b45024e171df3c19ef7af6161791ddc6c545
Rohini G B <gbrohini840@gmail.com>
2025-12-31 13:10:03 +0530

    ES6 arrow function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
+import MyComponent from './components/Greet'
 import React from 'react';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-       <Greet></Greet>
+       <MyComponent />
       </div>
     );
   }
