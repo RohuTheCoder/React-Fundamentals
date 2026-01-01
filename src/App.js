d5ee4c982257d71c756fefc1b3b4356bfc6865fc
Rohini G B <gbrohini840@gmail.com>
2026-01-01 11:47:42 +0530

    Hello Component With JSX

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Greet } from './components/Greet'
 import React from 'react';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-       <Greet />
-       <Welcome />
+       {/* <Greet /> */}
+       {/* <Welcome /> */}
+       <Hello />
       </div>
     );
   }
