80d8d891e472145cefeb2c8f76d8ec759fec4b62
Rohini G B <gbrohini840@gmail.com>
2026-01-05 14:30:37 +0530

    Conditional Rendering Using Element Variables

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,24 +6,26 @@ class UserGreeting extends Component {
         super(props)
 
         this.state = {
-            isLoggedIn: true
+            isLoggedIn: false
         }
     }
 
     render() {
+
+        let message
         if(this.state.isLoggedIn) {
-            return (
-                <div>
-                    Welcome Rohini
-                </div>
-            )
-        } else{
-            return (
-                <div>
-                    Welcome Guest
-                </div>
-            )
+            message = <div>Welcome Rohini</div>
+        } else {
+            message = <div>Welcome Guest</div>
         }
+
+        return <div>{message}</div>
+
+        // if(this.state.isLoggedIn) {
+        //     return <div>Welcome Rohini</div>
+        // } else{
+        //     return <div>Welcome Guest</div>
+        // }
         // return (
         //   <div>
         //        <div>Welcome Rohini</div>
