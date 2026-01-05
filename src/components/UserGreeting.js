de337ebd352c419cc47faf2b53a4ef483c1b18c3
Rohini G B <gbrohini840@gmail.com>
2026-01-05 14:14:55 +0530

    Conditional Rendering Using if else

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+
+    render() {
+        if(this.state.isLoggedIn) {
+            return (
+                <div>
+                    Welcome Rohini
+                </div>
+            )
+        } else{
+            return (
+                <div>
+                    Welcome Guest
+                </div>
+            )
+        }
+        // return (
+        //   <div>
+        //        <div>Welcome Rohini</div>
+        //        <div>Welcome Guest</div>
+        //    </div>
+        // )
+    }
+}
+
+export default UserGreeting
