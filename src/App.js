45a9be9d36b787bdfe8b2fd3db91f588b24df950
Rohini G B <gbrohini840@gmail.com>
2026-01-22 16:49:52 +0530

    Refs in Recat

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ParentComp />
+        <RefsDemo />
+       {/* <ParentComp /> */}
        {/* <PureComp /> */}
        {/* <Table /> */}
        {/* <FragmentDemo /> */}
