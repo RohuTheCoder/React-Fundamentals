b6e26f736634658c0cd94edc578439430dcb45e4
Rohini G B <gbrohini840@gmail.com>
2026-01-22 17:22:48 +0530

    Adding Refs to Class Components

--- src/App.js
+++ src/App.js
@@ -23,12 +23,14 @@ import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <RefsDemo />
+        <FocusInput />
+       {/* <RefsDemo /> */}
        {/* <ParentComp /> */}
        {/* <PureComp /> */}
        {/* <Table /> */}
