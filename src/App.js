fba246779482ff693363c35323125c78e3760012
Rohini G B <gbrohini840@gmail.com>
2026-01-23 13:57:28 +0530

    Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -24,12 +24,14 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <FocusInput />
+       <FRParentInput />
+       {/* <FocusInput /> */}
        {/* <RefsDemo /> */}
        {/* <ParentComp /> */}
        {/* <PureComp /> */}
