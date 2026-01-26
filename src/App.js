39cef2ea451a1941c08c8e8a40836805284440b4
Rohini G B <gbrohini840@gmail.com>
2026-01-26 13:39:48 +0530

    Portals in React

--- src/App.js
+++ src/App.js
@@ -25,12 +25,14 @@ import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-       <FRParentInput />
+        <PortalDemo />
+       {/* <FRParentInput /> */}
        {/* <FocusInput /> */}
        {/* <RefsDemo /> */}
        {/* <ParentComp /> */}
