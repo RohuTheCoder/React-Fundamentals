b88e7855c4f0c1f787eb5e2cbdb87ca6d4b496f1
Rohini G B <gbrohini840@gmail.com>
2026-01-29 12:39:49 +0530

    ClickCounter and HoverCounter

--- src/App.js
+++ src/App.js
@@ -1,75 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import React from 'react';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErrorBoundary from './ErrorBoundary';
+import React, { Component } from 'react';
+import './App.css'
+import ClickCounter from './ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ErrorBoundary>
-        <Hero heroName="Batman" />
-        <Hero heroName="Superman" />
-        <Hero heroName="Joker" />
-        </ErrorBoundary>
-       {/* <PortalDemo /> */}
-       {/* <FRParentInput /> */}
-       {/* <FocusInput /> */}
-       {/* <RefsDemo /> */}
-       {/* <ParentComp /> */}
-       {/* <PureComp /> */}
-       {/* <Table /> */}
-       {/* <FragmentDemo /> */}
-       {/* <LifecycleA /> */}
-       {/* <Form /> */}
-       {/* <h1 className='error'>Error</h1>
-       <h1 className={styles.success}>Success</h1> */}
-       {/* <Inline /> */}
-       {/* <Stylesheet primary={true}/> */}
-       {/* <NameList /> */}
-       {/* <UserGreeting /> */}
-       {/* <ParentComponent /> */}
-       {/* <EventBind /> */}
-       {/* <FunctionClick /> */}
-       {/* <ClassClick /> */}
-       {/* <Counter /> */}
-       {/* <Message /> */}
-       {/* <Greet name="Bruce" heroName="Batman" />
-        <p>This is children props</p>
-       <Greet name="Clark" heroName="Superman" />
-        <button>Action</button>
-        </Greet>*/}
-       {/* <Greet name="Diana" heroName="Wonder Woman" />
-        <Welcome name="Bruce" heroName="Batman" /> */}
-        {/* <Welcome name="Clark" heroName="Superman" />
-        <Welcome name="Diana" heroName="Wonder Woman" /> */}
-       {/* <Hello /> */}
+        <ClickCounter />
+        <HoverCounter />
       </div>
     );
   }
