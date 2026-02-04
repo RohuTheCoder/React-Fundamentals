5d2ad4e0b97a0f38a13b4945a958aeb53320f557
Rohini G B <gbrohini840@gmail.com>
2026-02-04 14:36:13 +0530

    HTTP GET Request

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css'
-import ComponentC from './components/ComponentC';
-import { UserProvider } from './components/userContext';
+import PostList from './components/PostList';
 
 
 class App extends React.Component {
   render() {
-    console.log('UserProvider in App:', UserProvider)
     return (
       <div className="App">
-        <UserProvider value="Vishwas">
-          <ComponentC />
-        </UserProvider>
+        <PostList />
       </div>
     );
   }
